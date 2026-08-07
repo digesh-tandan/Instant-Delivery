97d8d60217cc44dc8ce6f1aec08c485af1a92e61
Digesh Tandan <digeshtandan9212@gmail.com>
2026-08-07 13:36:24 +0530

    backend url updated in frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
 
         console.log(
-            `Server running on ${PORT}`
+            `Server running on port ${PORT}`
         );
 
     });
